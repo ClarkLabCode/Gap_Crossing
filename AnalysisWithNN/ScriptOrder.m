1dbeb9efa0d224c2b3060a1f021cef2d9f3ff7ed
jshomar <53152427+jshomar@users.noreply.github.com>
2021-09-22 14:28:03 -0400

    Notes of Scripts in this Folder

---
+++ AnalysisWithNN/ScriptOrder.m
@@ -0,0 +1,31 @@
+%% Order scripts to be used in
+  
+% GapCrossingAnalysis
+    % FindFramesForFlip
+    % RawFlyInfoExtracter
+    % CorridorIdentifier
+    % CompTracer
+    % CompIdentifier
+    % FinalStatsToFlyStruct
+    % FlyActivityFilter
+    % MetaDataAdder
+    % FlipBinner
+    % ZapStationaryFlips
+    % FindCrossEvents
+    
+% Post Processing
+    % CombineExpStructs
+    % CrossFrameIDMatMaker
+    % LabeledCrossingMatInputAndLabelMaker
+    % FallFinder
+  
+% Utilities
+    % SpeedVarianceFinder
+    % VisualizeTrajectoryOnCassette
+    
+% NeuralNet
+    % TrainingImgDataStoreMakerAll
+    % TrainingImgDataStoreMakerOffOnTrain
+    % TrainingImgDataStoreMakerAmbigUnambigTrain
+    % TrainGlassNNClassifier
+    % NNEvaluationTests
